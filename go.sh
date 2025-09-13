9046f49b83549310b44d709067746289d0c77104
Paul Gribble <pgribble@uwo.ca>
2025-09-13 10:34:48 -0400

    loss reorg

--- go.sh
+++ go.sh
@@ -1,9 +1,19 @@
 #!/bin/zsh
 
+# loss weights from Michaels 2025 Nature paper
+# Sensory expectations shape neural population dynamics in motor circuits
+
 python go.py \
-  --n_batch=20000 \
-  --batch_size=32 \
-  --interval=1000 \
+  --n_batch=5000 \
+  --batch_size=64 \
+  --interval=200 \
   --catch_trial_perc=50 \
-  --n_models=10 \
-  --dir_name=models
+  --n_models=4 \
+  --dir_name=models \
+  --loss_weight_position=1e+3 \
+  --loss_weight_speed=2e+2 \
+  --loss_weight_jerk=1e+6 \
+  --loss_weight_muscle=1e+0 \
+  --loss_weight_muscle_derivative=0 \
+  --loss_weight_hidden=1e-1 \
+  --loss_weight_hidden_derivative=1e+4
--- my_loss.py
+++ my_loss.py
@@ -1,7 +1,6 @@
 import numpy as np
 import torch as th
 
-
 def cal_loss(data, loss_weights=None):
 
     loss = {'position': None,
@@ -19,26 +18,7 @@ def cal_loss(data, loss_weights=None):
     loss['muscle']            = th.mean(th.sum(data['all_force'], dim=-1))
     loss['muscle_derivative'] = th.mean(th.sum(th.square(th.diff(data['all_force'], n=1, dim=1)), dim=-1))
     loss['hidden']            = th.mean(th.sum(th.square(data['all_hidden']), dim=-1))
-    loss['hidden_derivative'] = th.mean(th.sum(th.square(th.diff(data['all_hidden'], n=1, dim=1)), dim=-1))
-
-    if (loss_weights is None):
-        # from Kashefi et al 2025, Compositional neural dynamics during reaching 10.1101/2025.09.04.674069
-        # loss_weights = np.array([1e+0,   # position
-        #                          1e-3,   # speed
-        #                          1e-4,   # jerk
-        #                          1e-4,   # muscle
-        #                          1e-4,   # muscle_derivative
-        #                          1e-2,   # hidden
-        #                          1e-1])  # hidden_derivative
-        #
-        # from Shabazi et al 2024 A Context-Free Model of Savings in Motor Learning 10.1101/2025.03.26.645562
-        loss_weights = np.array([1e+3,   # position
-                                    0,   # speed
-                                 1e+5,   # jerk
-                                 1e-1,   # muscle
-                                 3e-4,   # muscle_derivative
-                                 1e-5,   # hidden
-                                 1e-3])  # hidden_derivative
+    loss['hidden_derivative'] = th.mean(th.sum(th.square(th.diff(data['all_hidden'], n=2, dim=1)), dim=-1)) # spectral note n=2
 
     losses_weighted = {
         'position':          loss_weights[0] * loss['position'],
@@ -55,4 +35,5 @@ def cal_loss(data, loss_weights=None):
         overall_loss += losses_weighted[loss]
 
     return overall_loss, losses_weighted
+
 
