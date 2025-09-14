2a0fc2e5922cbe96cd89a7b27c5fe368354ed966
Paul Gribble <pgribble@uwo.ca>
2025-09-14 11:56:54 -0400

    misc

--- go_kashefi.sh
+++ go_kashefi.sh
@@ -5,15 +5,15 @@ python go.py \
   --n_units=256 \
   --n_batch=20000 \
   --batch_size=256 \
-  --interval=200 \
+  --interval=500 \
   --catch_trial_perc=40 \
-  --n_models=3 \
-  --dir_name=models_kashefi \
+  --n_models=4 \
   --loss_weight_position=1e+0 \
   --loss_weight_speed=1e-3 \
   --loss_weight_jerk=1e-4 \
   --loss_weight_muscle=1e-4 \
   --loss_weight_muscle_derivative=1e-4 \
   --loss_weight_hidden=1e-2 \
-  --loss_weight_hidden_derivative=1e-1
+  --loss_weight_hidden_derivative=1e-1 \
+  --dir_name=models_kashefi
 
