8240792208e5fd9f7f785c4d154dfe8f39d36d0f
Paul Gribble <pgribble@uwo.ca>
2025-09-14 08:43:45 -0400

    misc

--- go_kashefi.sh
+++ go_kashefi.sh
@@ -1,22 +1,6 @@
 #!/bin/zsh
 # loss weights from Kashefi 2025 Compositional neural dynamics during reaching
 
-# python go.py \
-#   --n_units=256 \
-#   --n_batch=20000 \
-#   --batch_size=256 \
-#   --interval=200 \
-#   --catch_trial_perc=40 \
-#   --n_models=3 \
-#   --dir_name=models_kashefi \
-#   --loss_weight_position=1e+0 \
-#   --loss_weight_speed=1e-3 \
-#   --loss_weight_jerk=1e-4 \
-#   --loss_weight_muscle=1e-4 \
-#   --loss_weight_muscle_derivative=1e-4 \
-#   --loss_weight_hidden=1e-2 \
-#   --loss_weight_hidden_derivative=1e-1
-
 python go.py \
   --n_units=256 \
   --n_batch=20000 \
@@ -27,7 +11,7 @@ python go.py \
   --dir_name=models_kashefi \
   --loss_weight_position=1e+0 \
   --loss_weight_speed=1e-3 \
-  --loss_weight_jerk=1e-3 \
+  --loss_weight_jerk=1e-4 \
   --loss_weight_muscle=1e-4 \
   --loss_weight_muscle_derivative=1e-4 \
   --loss_weight_hidden=1e-2 \
