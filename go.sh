45155c997f719431e8eb1f0fa4f7d353db7894d6
Paul Gribble <pgribble@uwo.ca>
2025-09-13 21:53:33 -0400

    kashefi

--- go.sh
+++ go.sh
@@ -1,10 +1,3 @@
 #!/bin/zsh
 
-python go.py \
-  --n_batch=5000 \
-  --batch_size=64 \
-  --interval=200 \
-  --catch_trial_perc=50 \
-  --n_models=4 \
-  --n_units=256 \
-  --dir_name=models 
+./go_kashefi.sh
