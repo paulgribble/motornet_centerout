1a3825d8757fa3ddc5b1a6042e566c646b70b314
Paul Gribble <pgribble@uwo.ca>
2025-09-19 13:44:44 -0400

    misc

--- go_plg.sh
+++ go_plg.sh
@@ -1,19 +1,18 @@
 #!/bin/zsh
-# loss weights from Shabazi et al 2024 A Context-Free Model of Savings in Motor Learning 10.1101/2025.03.26.645562
 
 python go.py \
-  --n_batch=20000 \
+  --n_batch=5000 \
   --batch_size=64 \
-  --interval=1000 \
-  --catch_trial_perc=50 \
+  --interval=100 \
+  --catch_trial_perc=40 \
   --n_models=5 \
   --dir_name=models \
   --loss_weight_position=1e+3 \
-  --loss_weight_speed=1e-1 \
-  --loss_weight_jerk=1e+5 \
-  --loss_weight_muscle=1e-1 \
-  --loss_weight_muscle_derivative=1e-1 \
-  --loss_weight_hidden=1e-0 \
-  --loss_weight_hidden_derivative=1e+1
+  --loss_weight_speed=5e+2 \
+  --loss_weight_jerk=1e+4 \
+  --loss_weight_muscle=0 \
+  --loss_weight_muscle_derivative=0 \
+  --loss_weight_hidden=0 \
+  --loss_weight_hidden_derivative=0
 
 
