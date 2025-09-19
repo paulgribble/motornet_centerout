c17d0277898946a4ffa2a167a8acdcfd090557a2
Paul Gribble <pgribble@uwo.ca>
2025-09-19 12:23:36 -0400

    weights

--- go_plg.sh
+++ go_plg.sh
@@ -9,11 +9,11 @@ python go.py \
   --n_models=5 \
   --dir_name=models \
   --loss_weight_position=1e+3 \
-  --loss_weight_speed=1e-3 \
+  --loss_weight_speed=1e-1 \
   --loss_weight_jerk=1e+5 \
   --loss_weight_muscle=1e-1 \
   --loss_weight_muscle_derivative=1e-1 \
   --loss_weight_hidden=1e-0 \
-  --loss_weight_hidden_derivative=1e-0
+  --loss_weight_hidden_derivative=1e+1
 
 
