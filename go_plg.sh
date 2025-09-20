4d4f46131cdd69bf3456ec7af0167a4a3d1c29c5
Paul Gribble <pgribble@uwo.ca>
2025-09-20 16:35:16 -0400

    misc

--- go_plg.sh
+++ go_plg.sh
@@ -2,14 +2,14 @@
 #
 
 python go.py \
-  --n_batch=10000 \
+  --n_batch=5000 \
   --batch_size=64 \
   --interval=100 \
   --catch_trial_perc=50 \
   --n_models=5 \
   --dir_name=models \
   --loss_weight_position=1e+2 \
-  --loss_weight_speed=1e-2 \
+  --loss_weight_speed=1e-3 \
   --loss_weight_jerk=1e-3 \
   --loss_weight_muscle=1e-1 \
   --loss_weight_muscle_derivative=1e-0 \
