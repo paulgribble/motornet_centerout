91ec9a6c0ad011e9816b4424f82054d5c7aa50d7
Paul Gribble <pgribble@uwo.ca>
2025-09-21 11:28:01 -0400

    misc

--- go_plg.sh
+++ go_plg.sh
@@ -2,18 +2,18 @@
 #
 
 python go.py \
-  --n_batch=10000 \
-  --batch_size=128 \
+  --n_units=256 \
+  --n_batch=20000 \
+  --batch_size=64 \
   --interval=1000 \
   --catch_trial_perc=50 \
   --n_models=5 \
   --dir_name=models \
-  --loss_weight_position=1e+1 \
-  --loss_weight_speed=1e-3 \
-  --loss_weight_jerk=1e+3 \
-  --loss_weight_muscle=5e-4 \
-  --loss_weight_muscle_derivative=1e-3 \
-  --loss_weight_hidden=1e-1 \
-  --loss_weight_hidden_derivative=1e-0
-
+  --loss_weight_position=1e+2 \
+  --loss_weight_speed=1e-1 \
+  --loss_weight_jerk=1e-2 \
+  --loss_weight_muscle=1e-2 \
+  --loss_weight_muscle_derivative=1e-2 \
+  --loss_weight_hidden=1e-0 \
+  --loss_weight_hidden_derivative=1e+1
 
